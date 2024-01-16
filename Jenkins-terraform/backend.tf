6645adc530db7556ee5cf9f0a92e569a92f4c1d0
Maharshi Bhatnagar <119435144+Maharshibhatnagar@users.noreply.github.com>
2024-01-16 16:25:53 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "tetris-is-not-for-me" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
