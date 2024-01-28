df5e219d09216674478778a22433c3fd58b2dd06
Maharshi Bhatnagar <119435144+Maharshibhatnagar@users.noreply.github.com>
2024-01-28 16:37:03 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tetris-is-not-for-me" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "eks/terraform.tfstate" # Major change i got errors 3 time while building bcoz i was using same key for both the builds
     region = "ap-south-1"
   }
 }
